fc7cf23b16fc20d1ebee35f499116af299540d1f
gufronnakaaw <gufronnakaaw@gmail.com>
2024-09-17 12:21:37 +0700

    add figure images

---
+++ utils/quotes.ts
@@ -0,0 +1,37 @@
+export const quotes = [
+  {
+    quote:
+      "Tujuan pendidikan itu untuk mempertajam kecerdasan, memperkukuh kemauan, serta memperhalus perasaan.",
+    figure: "Tan Malaka",
+    image: "/img/tan-malaka.jpg",
+  },
+  {
+    quote:
+      "Hanya pendidikan yang bisa menyelamatkan masa depan, tanpa pendidikan Indonesia tak mungkin bertahan.",
+    figure: "Najwa Shihab",
+    image: "/img/najwa-shihab.jpg",
+  },
+  {
+    quote:
+      "Apapun yang dilakukan oleh seseorang itu, hendaknya dapat bermanfaat bagi dirinya sendiri, bermanfaat bagi bangsanya, dan bermanfaat bagi manusia di dunia pada umumnya.",
+    figure: "Ki Hadjar Dewantara",
+    image: "/img/ki-hajar-dewantara.jpg",
+  },
+  {
+    quote:
+      "Keberhasilan bukanlah milik orang yang pintar. Keberhasilan adalah kepunyaan mereka yang senantiasa berusaha.",
+    figure: "BJ Habibie",
+    image: "/img/bj-habibie.jpg",
+  },
+  {
+    quote:
+      "Berikan aku 1.000 orang tua, niscaya akan kucabut Semeru dari akarnya. Berikan aku 10 pemuda, niscaya akan kuguncangkan dunia.",
+    figure: "Ir. Soekarno",
+    image: "/img/ir-soekarno.jpg",
+  },
+  {
+    quote: "Obat yang paling mujarab adalah ilmu pengetahuan dan kasih sayang.",
+    figure: "KH. Agus Salim",
+    image: "/img/kh-agus-salim.jpg",
+  },
+];
