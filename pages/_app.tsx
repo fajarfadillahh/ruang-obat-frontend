bad4fb5f45cd52946f43f337e48e7409d4ce9975
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-09-27 10:26:21 +0700

    setup Toaster on _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Toaster
+        toastOptions={{
+          style: {
+            fontFamily: "Mulish, sans-serif",
+            fontWeight: 600,
+            color: "#171717",
+          },
+        }}
+      />
       <Component {...pageProps} />
     </NextUIProvider>
   );
