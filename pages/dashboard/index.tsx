6cce7244295810c19d121e3e8b80fbc79c294509
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-09-27 12:04:53 +0700

    add formatDate utils & added time on dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,21 +1,43 @@
 import { programs } from "@/_dummy/programs";
 import CardProgram from "@/components/card/CardProgram";
 import Layout from "@/components/wrapper/Layout";
+import { formatDayWithoutTime } from "@/utils/formatDate";
 import { Input, Select, SelectItem } from "@nextui-org/react";
 import { MagnifyingGlass } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
-  const filterPrograms = [
-    { key: "free", label: "Gratis" },
-    { key: "paid", label: "Berbayar" },
-  ];
+  const [time, setTime] = useState(new Date());
+  const [client, setClient] = useState(false);
+  const formatTime = (num: any) => String(num).padStart(2, "0");
+
+  useEffect(() => {
+    setClient(true);
+
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!client) {
+    return;
+  }
 
   return (
     <Layout title="Pilih Program Yang Sesuai Dengan Kebutuhan Anda">
       <section className="grid gap-6 pt-8">
-        <h1 className="text-[24px] font-extrabold -tracking-wide text-black">
-          Daftar Program 📋
-        </h1>
+        <div className="flex items-center justify-between gap-2">
+          <h1 className="text-[24px] font-extrabold -tracking-wide text-black">
+            Daftar Program 📋
+          </h1>
+
+          <p className="text-sm font-semibold text-black">
+            {formatDayWithoutTime(new Date())}{" "}
+            {`${formatTime(time.getHours())}:${formatTime(time.getMinutes())}`}
+          </p>
+        </div>
 
         <div className="grid gap-4">
           <div className="flex items-center justify-between gap-4">
@@ -40,7 +62,6 @@ export default function DashboardPage() {
             <Select
               aria-label="filter program"
               variant="flat"
-              items={filterPrograms}
               placeholder="Filter Program"
               labelPlacement="outside"
               classNames={{
@@ -48,9 +69,8 @@ export default function DashboardPage() {
                 value: "font-semibold text-black",
               }}
             >
-              {(program) => (
-                <SelectItem key={program.key}>{program.label}</SelectItem>
-              )}
+              <SelectItem key="free">Gratis</SelectItem>
+              <SelectItem key="paid">Berbayar</SelectItem>
             </Select>
           </div>
 
