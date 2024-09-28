fe7876c51470ab4195f64569eb2318d2c088e766
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-09-28 15:34:15 +0700

    responsive details test page

--- pages/tests/[id]/index.tsx
+++ pages/tests/[id]/index.tsx
@@ -13,7 +13,7 @@ export default function DetailsTest() {
       <section className="grid gap-8 pt-8">
         <ButtonBack />
 
-        <div className="grid grid-cols-[1fr_300px] items-end justify-between gap-4">
+        <div className="grid items-end justify-center gap-10 lg:grid-cols-[1fr_300px] lg:justify-between lg:gap-4">
           <div className="grid max-w-[750px] divide-y-2 divide-dashed divide-gray/20">
             <div className="grid gap-4 pb-8">
               <h4 className="text-[28px] font-bold capitalize leading-[120%] -tracking-wide text-black">
@@ -27,33 +27,37 @@ export default function DetailsTest() {
               </p>
             </div>
 
-            <div className="flex items-start justify-between gap-4 pt-8">
+            <div className="grid gap-4 pt-8 xs:grid-cols-2 sm:grid-cols-3 md:flex md:items-start md:justify-between">
               <div className="grid gap-1">
                 <span className="text-sm font-medium text-gray">
                   Tanggal Mulai:
                 </span>
-                <h1 className="font-bold text-black">5 Agustus 2024 10:00</h1>
+                <h1 className="font-semibold text-black">
+                  5 Agustus 2024 10:00
+                </h1>
               </div>
 
               <div className="grid gap-1">
                 <span className="text-sm font-medium text-gray">
                   Tanggal Selesai:
                 </span>
-                <h1 className="font-bold text-black">10 Agustus 2024 23:59</h1>
+                <h1 className="font-semibold text-black">
+                  10 Agustus 2024 23:59
+                </h1>
               </div>
 
               <div className="grid gap-1">
                 <span className="text-sm font-medium text-gray">
                   Durasi Pengerjaan:
                 </span>
-                <h1 className="font-bold text-black">200 Menit</h1>
+                <h1 className="font-semibold text-black">200 Menit</h1>
               </div>
 
               <div className="grid gap-1">
                 <span className="text-sm font-medium text-gray">
                   Jumlah Soal:
                 </span>
-                <h1 className="font-bold text-black">100 Butir</h1>
+                <h1 className="font-semibold text-black">100 Butir</h1>
               </div>
             </div>
           </div>
@@ -70,7 +74,7 @@ export default function DetailsTest() {
                 startContent={<ClockCountdown weight="bold" size={18} />}
                 classNames={{
                   base: "px-3 gap-1",
-                  content: "font-bold",
+                  content: "font-semibold",
                 }}
               >
                 Belum Dimulai
