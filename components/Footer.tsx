3c5e3a0e94228e70971f9ca2e1f986423b889492
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-10-01 10:51:57 +0700

    modified footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,9 @@
-import Link from "next/link";
-
 export default function Footer() {
   return (
-    <footer className="flex flex-wrap items-center justify-center gap-4 border-t-2 border-gray/20 p-6 text-center xs:justify-between">
+    <footer className="flex items-center justify-center gap-4 border-t-2 border-gray/10 p-6 text-center">
       <p className="text-sm font-medium capitalize text-gray">
         &copy; PT. Pharmacy Cone Group {new Date().getFullYear()}
       </p>
-
-      <div className="inline-flex items-center gap-4">
-        <Link
-          href="/company/terms"
-          className="text-[12px] font-medium text-gray underline hover:text-purple"
-        >
-          Ketentuan Layanan
-        </Link>
-
-        <Link
-          href="/company/privacy"
-          className="text-[12px] font-medium text-gray underline hover:text-purple"
-        >
-          Kebijakan Privasi
-        </Link>
-      </div>
     </footer>
   );
 }
