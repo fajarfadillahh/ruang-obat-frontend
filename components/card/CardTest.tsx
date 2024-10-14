dcb3f59409617b3753d31dc98307a9ccc84d98b9
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-10-14 22:44:19 +0700

    modified CardTest component when is_active condition

--- components/card/CardTest.tsx
+++ components/card/CardTest.tsx
@@ -1,18 +1,41 @@
 import { formatDateWithoutTime } from "@/utils/formatDate";
 import { Button, Chip } from "@nextui-org/react";
-import { ClipboardText, HourglassLow, Lock } from "@phosphor-icons/react";
+import {
+  CheckCircle,
+  ClipboardText,
+  HourglassLow,
+  Lock,
+  Prohibit,
+  XCircle,
+} from "@phosphor-icons/react";
 import { useRouter } from "next/router";
 
 export default function CardTest(test: CardTest) {
   const router = useRouter();
 
   return (
-    <div className="group grid gap-6 rounded-xl border-2 border-purple/10 bg-transparent p-6 hover:border-purple hover:bg-purple/10 md:grid-cols-[1fr_max-content] md:items-center">
+    <div
+      className={`group grid gap-6 rounded-xl border-2 bg-transparent p-6 md:grid-cols-[1fr_max-content] md:items-center ${
+        test.is_active
+          ? "border-purple/10 hover:border-purple hover:bg-purple/10"
+          : "border-danger bg-danger/5 hover:bg-danger/10"
+      }`}
+    >
       <div className="flex flex-1 items-start gap-3">
-        <ClipboardText weight="bold" size={28} className="text-purple" />
+        {test.is_active ? (
+          <ClipboardText weight="bold" size={28} className="text-purple" />
+        ) : (
+          <Prohibit weight="bold" size={28} className="text-danger" />
+        )}
 
         <div className="grid flex-1 gap-6">
-          <h4 className="line-clamp-2 max-w-[620px] text-[20px] font-bold leading-[120%] -tracking-wide text-black group-hover:text-purple">
+          <h4
+            className={`line-clamp-2 max-w-[620px] text-[20px] font-bold leading-[120%] -tracking-wide ${
+              test.is_active
+                ? "text-black group-hover:text-purple"
+                : "text-danger"
+            }`}
+          >
             {test.title}
           </h4>
 
@@ -49,17 +72,39 @@ export default function CardTest(test: CardTest) {
                 Status Ujian:
               </span>
 
-              <Chip
-                variant="flat"
-                color="warning"
-                startContent={<HourglassLow weight="fill" size={18} />}
-                classNames={{
-                  base: "px-2",
-                  content: "font-semibold capitalize",
-                }}
-              >
-                {test.status}
-              </Chip>
+              <div className="inline-flex items-center gap-2">
+                <Chip
+                  variant="flat"
+                  color="warning"
+                  size="sm"
+                  startContent={<HourglassLow weight="fill" size={18} />}
+                  classNames={{
+                    base: "px-2",
+                    content: "font-semibold capitalize",
+                  }}
+                >
+                  {test.status}
+                </Chip>
+
+                <Chip
+                  variant="flat"
+                  color={test.is_active ? "success" : "danger"}
+                  size="sm"
+                  startContent={
+                    test.is_active ? (
+                      <CheckCircle weight="fill" size={16} />
+                    ) : (
+                      <XCircle weight="fill" size={16} />
+                    )
+                  }
+                  classNames={{
+                    base: "px-2 gap-1",
+                    content: "font-semibold capitalize",
+                  }}
+                >
+                  {test.is_active ? "Aktif" : "Tidak Aktif"}
+                </Chip>
+              </div>
             </div>
           </div>
         </div>
@@ -68,10 +113,10 @@ export default function CardTest(test: CardTest) {
       <Button
         variant="solid"
         size="sm"
-        color="secondary"
+        color={!test.is_active ? "danger" : "secondary"}
         onClick={() => router.push(`/tests/${test.test_id}`)}
         className="w-full font-bold md:w-max md:px-6"
-        isDisabled={!test.is_approved}
+        isDisabled={!test.is_approved || !test.is_active}
       >
         {!test.is_approved ? <Lock weight="bold" size={18} /> : "Lihat Ujian"}
       </Button>
