3e00c2fa6d0fb1ecb6422e85c68ffc46ebfc8f9b
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2024-12-09 23:15:25 +0700

    refactor ButtonBack component to accept optional href prop and update router behavior accordingly. Update ButtonBack usage in my/programs.tsx and my/tests.tsx to use the new behavior.

--- components/button/ButtonBack.tsx
+++ components/button/ButtonBack.tsx
@@ -2,14 +2,22 @@ import { Button } from "@nextui-org/react";
 import { ArrowLeft } from "@phosphor-icons/react";
 import { useRouter } from "next/router";
 
-export default function ButtonBack() {
+type ButtonBackProps = {
+  href?: string;
+};
+
+export default function ButtonBack({ href }: ButtonBackProps) {
   const router = useRouter();
+
+  const handleClick = () => {
+    href ? router.push(href) : router.back();
+  };
 
   return (
     <Button
       variant="light"
       startContent={<ArrowLeft weight="bold" size={16} />}
-      onClick={() => router.back()}
+      onClick={handleClick}
       className="w-max px-4 font-bold text-black"
     >
       Kembali
