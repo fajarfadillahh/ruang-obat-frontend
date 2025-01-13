75fe2149cd8b442dde077b34669aac03d218c5da
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-01-13 12:40:52 +0700

    implemented get details mentor

--- types/homepage.type.ts
+++ types/homepage.type.ts
@@ -5,3 +5,13 @@ export type MentorType = {
   mentor_title: string;
   img_url: string;
 };
+
+export type MentorDetailsType = {
+  mentor_id: string;
+  fullname: string;
+  nickname: string;
+  mentor_title: string;
+  description: string;
+  img_url: string;
+  created_at: Date;
+};
