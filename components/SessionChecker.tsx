f00f78d2f72aeeff6e448c41d34ed2f0babc750b
gufronnakaaw <gufronnakaaw@gmail.com>
2025-05-11 21:28:29 +0700

    refactor: update SessionChecker to check email verification status

--- components/SessionChecker.tsx
+++ components/SessionChecker.tsx
@@ -1,9 +1,15 @@
-import { getSession, signOut } from "next-auth/react";
+import { SuccessResponse } from "@/types/global.type";
+import { UserDataResponse } from "@/types/profile.type";
+import { fetcher } from "@/utils/fetcher";
+import { getSession, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function SessionChecker() {
   const router = useRouter();
+  const { data: session, status } = useSession();
+  const [hasChecked, setHasChecked] = useState(false);
 
   async function checkSession() {
     const session = await getSession();
@@ -14,6 +20,21 @@ export default function SessionChecker() {
 
     if (now > expired) {
       return signOut();
+    }
+  }
+
+  async function checkVerified(token: string) {
+    try {
+      const response: SuccessResponse<UserDataResponse> = await fetcher({
+        url: "/my/profile",
+        method: "GET",
+        token,
+      });
+
+      return response.data.is_verified;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
   }
 
@@ -56,6 +77,30 @@ export default function SessionChecker() {
       window.onfocus = null;
     };
   }, [router]);
+
+  useEffect(() => {
+    if (status === "authenticated" && !hasChecked) {
+      checkStatus();
+    }
+
+    async function checkStatus() {
+      const is_verified = await checkVerified(
+        session?.user.access_token as string,
+      );
+
+      if (is_verified === false && router.pathname !== "/unverified") {
+        router.push("/unverified");
+      }
+
+      if (is_verified === null) {
+        toast.error("Gagal mengambil data user");
+      }
+
+      if (is_verified === true) {
+        setHasChecked(true);
+      }
+    }
+  }, [router, status, session, hasChecked]);
 
   return null;
 }
