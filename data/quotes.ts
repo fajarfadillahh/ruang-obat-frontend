721870eba638b8d8e39ef6a3cfff88152107100a
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-06-11 21:40:16 +0700

    chore: move all pahlawan image into the folder & change format to .webp | move file quotes into the data folder | adjustment login & register page

--- data/quotes.ts
+++ data/quotes.ts
@@ -3,45 +3,45 @@ export const quotes = [
     quote:
       "Tujuan pendidikan itu untuk mempertajam kecerdasan, memperkukuh kemauan, serta memperhalus perasaan.",
     figure: "Tan Malaka",
-    image: "/img/tan-malaka.jpg",
+    image: "/img/pahlawan/tan-malaka.webp",
   },
   {
     quote:
       "Hanya pendidikan yang bisa menyelamatkan masa depan, tanpa pendidikan Indonesia tak mungkin bertahan.",
     figure: "Najwa Shihab",
-    image: "/img/najwa-shihab.jpg",
+    image: "/img/pahlawan/najwa-shihab.webp",
   },
   {
     quote:
       "Apapun yang dilakukan oleh seseorang itu, hendaknya dapat bermanfaat bagi dirinya sendiri, bermanfaat bagi bangsanya, dan bermanfaat bagi manusia di dunia pada umumnya.",
     figure: "Ki Hadjar Dewantara",
-    image: "/img/ki-hajar-dewantara.jpg",
+    image: "/img/pahlawan/ki-hajar-dewantara.webp",
   },
   {
     quote:
       "Keberhasilan bukanlah milik orang yang pintar. Keberhasilan adalah kepunyaan mereka yang senantiasa berusaha.",
     figure: "BJ Habibie",
-    image: "/img/bj-habibie.jpg",
+    image: "/img/pahlawan/bj-habibie.webp",
   },
   {
     quote:
       "Berikan aku 1.000 orang tua, niscaya akan kucabut Semeru dari akarnya. Berikan aku 10 pemuda, niscaya akan kuguncangkan dunia.",
     figure: "Ir. Soekarno",
-    image: "/img/ir-soekarno.jpg",
+    image: "/img/pahlawan/ir-soekarno.webp",
   },
   {
     quote: "Obat yang paling mujarab adalah ilmu pengetahuan dan kasih sayang.",
     figure: "KH. Agus Salim",
-    image: "/img/kh-agus-salim.jpg",
+    image: "/img/pahlawan/kh-agus-salim.webp",
   },
   {
     quote: "Hanya dengan pendidikan kita bisa tumbuh menjadi suatu bangsa.",
     figure: "Dewi Sartika",
-    image: "/img/dewi-sartika.jpg",
+    image: "/img/pahlawan/dewi-sartika.webp",
   },
   {
     quote: "Habis gelap, terbitlah terang.",
     figure: "R.A Kartini",
-    image: "/img/ra-kartini.jpg",
+    image: "/img/pahlawan/ra-kartini.webp",
   },
 ];
