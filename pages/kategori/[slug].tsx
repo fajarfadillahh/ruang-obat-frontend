038dc857354e92cf4f4f7d68796240617c850cf1
gufronnakaaw <gufronnakaaw@gmail.com>
2025-06-30 00:47:00 +0700

    feat(sub_categories): implement data fetching

--- pages/kategori/[slug].tsx
+++ pages/kategori/[slug].tsx
@@ -2,60 +2,74 @@ import ButtonBack from "@/components/button/ButtonBack";
 import CTASecondary from "@/components/cta/CTASecondary";
 import Footer from "@/components/footer/Footer";
 import Layout from "@/components/wrapper/Layout";
-import { dummySubCategories } from "@/data/dummy";
-import { Asclepius, Heartbeat } from "@phosphor-icons/react";
+import { SuccessResponse } from "@/types/global.type";
+import { fetcher } from "@/utils/fetcher";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-export default function CategoryPage() {
-  const router = useRouter();
-  const { slug } = router.query;
+type CategoryDetailResponse = {
+  category_id: string;
+  name: string;
+  slug: string;
+  img_url: string;
+  type: string;
+  sub_categories: {
+    sub_category_id: string;
+    name: string;
+    slug: string;
+    img_url: string;
+  }[];
+};
 
-  const decodedSlug = decodeURIComponent(slug as string)
-    .replace(/-/g, " ")
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-
+export default function CategoryPage({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-      <Layout title={decodedSlug}>
+      <Layout title={data?.name as string}>
         <ButtonBack />
 
         {/* for type: videocourse & videoukmppai */}
         <section className="base-container gap-8 [padding:50px_0_100px]">
           <div className="flex items-center gap-4">
-            <Heartbeat
-              weight="duotone"
-              className="text-5xl text-purple xl:text-7xl"
-            />
+            <div className="relative h-16 w-16">
+              <Image
+                src={data?.img_url as string}
+                alt={data?.name as string}
+                className="object-cover"
+                fill
+              />
+            </div>
 
             <h1 className="flex-1 text-3xl font-black text-black xl:text-4xl">
-              {decodedSlug}
+              {data?.name as string}
             </h1>
           </div>
 
           <div className="grid grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-5">
-            {dummySubCategories.map((item, index) => (
+            {data?.sub_categories.map((sub_category) => (
               <Link
-                key={index}
-                href={`/materi/${item.subcategory_slug}?type=videocourse`}
+                key={sub_category.sub_category_id}
+                href={`/materi/${sub_category.slug}?type=videocourse`}
                 className="group grid justify-items-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 text-sm [padding:2rem_1rem] hover:cursor-pointer hover:bg-purple/10 sm:text-base"
               >
-                <Asclepius
-                  weight="duotone"
-                  className="text-5xl text-purple sm:text-6xl"
-                />
+                <div className="relative h-20 w-20">
+                  <Image
+                    src={sub_category.img_url}
+                    alt={sub_category.name}
+                    className="object-cover"
+                    fill
+                  />
+                </div>
 
                 <h4 className="line-clamp-2 text-center font-extrabold text-black group-hover:line-clamp-none">
-                  {item.subcategory_name}
+                  {sub_category.name}
                 </h4>
               </Link>
             ))}
           </div>
         </section>
-
-        {/* for type: apotekerclass */}
 
         <CTASecondary />
       </Layout>
@@ -64,3 +78,42 @@ export default function CategoryPage() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  data?: CategoryDetailResponse;
+  error?: any;
+}> = async ({ req, params, query }) => {
+  const slug = params?.slug as string;
+  const type = query.type as string;
+
+  if (!["apotekerclass", "videocourse", "videoukmppai"].includes(type)) {
+    return {
+      props: {
+        error: {
+          message: "Ups sepertinya parameter web tidak valid",
+        },
+      },
+    };
+  }
+
+  try {
+    const response: SuccessResponse<CategoryDetailResponse> = await fetcher({
+      url: `/categories/${slug}/${type}/detail`,
+      method: "GET",
+    });
+
+    return {
+      props: {
+        data: response.data,
+      },
+    };
+  } catch (error: any) {
+    console.error(error);
+
+    return {
+      props: {
+        error,
+      },
+    };
+  }
+};
