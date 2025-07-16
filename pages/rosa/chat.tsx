63ce5a375f4272268b71203f3663066eec55161f
gufronnakaaw <gufronnakaaw@gmail.com>
2025-07-16 17:55:48 +0700

    refactor(rosa): implement math formula & update codebase

--- pages/rosa/chat.tsx
+++ pages/rosa/chat.tsx
@@ -1,10 +1,11 @@
+import { MemoizedMarkdown } from "@/components/MemoizedMarkdown";
 import ModalUnauthenticated from "@/components/modal/ModalUnauthenticated";
 import NavbarMenu from "@/components/navbar/NavbarMenu";
 import { ChatInput } from "@/components/rosa/ChatInput";
+import ChatLoading from "@/components/rosa/ChatLoading";
 import ChatWelcome from "@/components/rosa/ChatWelcome";
 import { SuccessResponse } from "@/types/global.type";
 import { fetcher } from "@/utils/fetcher";
-import { loadingTexts } from "@/utils/loadingTexts";
 import { Button, useDisclosure } from "@nextui-org/react";
 import { ArrowUp, Paperclip, X } from "@phosphor-icons/react";
 import { useSession } from "next-auth/react";
@@ -12,10 +13,8 @@ import { NextSeo } from "next-seo";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 import useSWR from "swr";
 
 type CheckResponse = {
@@ -24,16 +23,17 @@ type CheckResponse = {
 };
 
 type ChatResponse = {
-  role: string;
+  role: "user" | "assistant";
   content: string;
   images?: { image_id: string; img_url: string }[];
+  chat_id: string;
 };
 
 type MessageState = {
-  role: string;
+  role: "user" | "assistant";
   content: string;
   is_loading?: boolean;
-  id?: number;
+  chat_id?: string;
   images?: { image_id: string; img_url: string }[];
 };
 
@@ -79,7 +79,7 @@ export default function RosaChatPage() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState<MessageState[]>([]);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [images, setImages] = useState<File[]>([]);
+  const [imageUrls, setImageUrls] = useState<{ url: string }[]>([]);
 
   useEffect(() => {
     if (status === "unauthenticated") {
@@ -90,162 +90,13 @@ export default function RosaChatPage() {
   useEffect(() => {
     if (chats?.data?.length) {
       setMessages(chats.data);
+      setTimeout(() => {
+        divRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 250);
     }
   }, [chats?.data]);
 
-  useEffect(() => {
-    if (messages.length) {
-      divRef.current?.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [messages]);
-
-  const handleFileUpload = useCallback(
-    async function (images: File[]) {
-      try {
-        const form = new FormData();
-
-        for (const image of images) {
-          form.append("images", image);
-        }
-
-        const result: SuccessResponse<{ url: string }[]> = await fetcher({
-          url: "/ai/chat/images",
-          method: "POST",
-          data: form,
-          file: true,
-          token: data?.user.access_token,
-        });
-
-        return result.data;
-      } catch (error) {
-        console.log(error);
-        return "Ups sepertinya ada kesalahan ketika upload gambar" as string;
-      }
-    },
-    [data?.user.access_token],
-  );
-
-  const handleSubmitChat = useCallback(async () => {
-    setInput("");
-    const id = Date.now();
-    if (!user?.data.remaining) return;
-
-    const newImages: { url: string }[] = [];
-
-    if (images.length) {
-      const upload = await handleFileUpload(images);
-
-      if (!Array.isArray(upload)) {
-        return toast.error(upload as string);
-      }
-
-      newImages.push(...upload);
-      setImages([]);
-    }
-
-    setMessages((prev) => [
-      ...prev,
-      {
-        role: "user",
-        content: input,
-        images: newImages.length
-          ? newImages.map((item) => ({
-              image_id:
-                Date.now().toString() +
-                Math.random().toString(36).substring(2, 15),
-              img_url: item.url,
-            }))
-          : [],
-      },
-      { role: "assistant", content: "", is_loading: true, id },
-    ]);
-
-    try {
-      const prefix = process.env.NEXT_PUBLIC_MODE == "dev" ? "dev" : "api";
-
-      const response = await fetch(
-        `https://${prefix}.ruangobat.id/api/ai/chat/streaming`,
-        {
-          headers: {
-            Authorization: `Bearer ${data?.user.access_token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            input,
-            img_url: newImages.length ? newImages.map((item) => item.url) : [],
-          }),
-          method: "POST",
-        },
-      );
-
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder();
-      let buffer = "";
-
-      while (true) {
-        const { done, value } = await reader?.read();
-        if (done) break;
-
-        buffer += decoder.decode(value, { stream: true });
-
-        let newlineIndex;
-        while ((newlineIndex = buffer.indexOf("\n")) >= 0) {
-          const line = buffer.slice(0, newlineIndex).trim();
-          buffer = buffer.slice(newlineIndex + 1);
-
-          if (line.startsWith("data:")) {
-            const data = line.replace(/^data:\s?/, "");
-
-            if (data === "[DONE]") {
-              reader?.cancel();
-              mutate();
-              mutateChat();
-              break;
-            }
-
-            setMessages((prev) =>
-              prev.map((msg) =>
-                msg.id === id
-                  ? {
-                      ...msg,
-                      role: "assistant",
-                      content: msg.content + data,
-                      is_loading: false,
-                    }
-                  : msg,
-              ),
-            );
-          }
-        }
-      }
-    } catch (error) {
-      console.error(error);
-
-      setMessages((prev) =>
-        prev.map((msg) =>
-          msg.id === id
-            ? {
-                role: "assistant",
-                content:
-                  "Ups sepertinya server kita ada masalah. Maaf ya atas ketidaknyamanannya 😫",
-                is_loading: false,
-                id: 0,
-              }
-            : msg,
-        ),
-      );
-    }
-  }, [
-    user?.data.remaining,
-    data?.user.access_token,
-    input,
-    mutate,
-    images,
-    mutateChat,
-    handleFileUpload,
-  ]);
-
-  function handleFileInput(e: ChangeEvent<HTMLInputElement>) {
+  async function handleFileUpload(e: ChangeEvent<HTMLInputElement>) {
     const files = e.target.files;
     if (!files || files.length === 0) return;
 
@@ -274,7 +125,192 @@ export default function RosaChatPage() {
       validFiles.push(file);
     });
 
-    setImages(validFiles);
+    try {
+      const form = new FormData();
+
+      for (const validFile of validFiles) {
+        form.append("images", validFile);
+      }
+
+      const result: SuccessResponse<{ url: string }[]> = await toast.promise(
+        fetcher({
+          url: "/ai/chat/images",
+          method: "POST",
+          data: form,
+          file: true,
+          token: data?.user.access_token,
+        }),
+        {
+          loading: "Sedang mengupload gambar...",
+          success: "Gambar berhasil diupload",
+          error: "Ups sepertinya ada kesalahan ketika upload gambar",
+        },
+      );
+
+      if (imageUrls.length) {
+        setImageUrls((prev) => [...prev, ...result.data]);
+      } else {
+        setImageUrls(result.data);
+      }
+    } catch (error) {
+      setImageUrls([]);
+      console.log(error);
+      toast.error("Ups sepertinya ada kesalahan ketika upload gambar");
+    }
+  }
+
+  async function handleSubmitChat() {
+    if (!user?.data.remaining) return;
+
+    setInput("");
+    const images = [...imageUrls];
+    setImageUrls([]);
+
+    const id = `${Date.now()}-${Math.random().toString(36).substring(2, 15)}`;
+
+    const chat_id_assistant = `${id}-assistant`;
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        chat_id: `${id}-user`,
+        role: "user",
+        content: input,
+        images: images.length
+          ? images.map((item) => ({
+              image_id:
+                Date.now().toString() +
+                Math.random().toString(36).substring(2, 15),
+              img_url: item.url,
+            }))
+          : [],
+      },
+      {
+        role: "assistant",
+        content: "",
+        is_loading: true,
+        id,
+        chat_id: chat_id_assistant,
+      },
+    ]);
+
+    setTimeout(() => {
+      divRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 250);
+
+    try {
+      const prefix = process.env.NEXT_PUBLIC_MODE === "prod" ? "api" : "dev";
+
+      const response = await fetch(
+        `https://${prefix}.ruangobat.id/api/ai/chat/streaming`,
+        {
+          headers: {
+            Authorization: `Bearer ${data?.user.access_token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            input,
+            img_url: images.length ? images.map((item) => item.url) : [],
+          }),
+          method: "POST",
+        },
+      );
+
+      const reader = response.body?.getReader();
+
+      if (!reader) {
+        return toast.error("Gagal mendapatkan pembaca dari response body");
+      }
+
+      const decoder = new TextDecoder();
+      let buffer = "";
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        buffer += decoder.decode(value, { stream: true });
+
+        let newlineIndex;
+        while ((newlineIndex = buffer.indexOf("\n")) >= 0) {
+          const line = buffer.slice(0, newlineIndex).trim();
+          buffer = buffer.slice(newlineIndex + 1);
+
+          if (line.startsWith("data:")) {
+            const data = line.replace(/^data:\s?/, "");
+
+            if (data === "[DONE]") {
+              reader?.cancel();
+              mutate();
+              mutateChat();
+              divRef.current?.scrollIntoView({ behavior: "smooth" });
+              break;
+            }
+
+            try {
+              const parsed = JSON.parse(data);
+              const content = parsed.content;
+              setMessages((prev) =>
+                prev.map((msg) =>
+                  msg.chat_id === chat_id_assistant
+                    ? {
+                        ...msg,
+                        role: "assistant",
+                        content: msg.content + content,
+                        is_loading: false,
+                      }
+                    : msg,
+                ),
+              );
+            } catch (error) {
+              // skip json parse error
+              console.error(error);
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+
+      setMessages((prev) =>
+        prev.map((msg) =>
+          msg.chat_id === chat_id_assistant
+            ? {
+                role: "assistant",
+                content:
+                  "Ups sepertinya server kita ada masalah. Maaf ya atas ketidaknyamanannya 😫",
+                is_loading: false,
+                id: 0,
+              }
+            : msg,
+        ),
+      );
+    }
+  }
+
+  async function handleDeleteImage(url: string) {
+    const urlObj = new URL(url);
+    const path = urlObj.pathname;
+    const cleanPath = path.startsWith("/") ? path.slice(1) : path;
+
+    await toast.promise(
+      fetcher({
+        url: `/ai/chat/images?key=${cleanPath}`,
+        method: "DELETE",
+        token: data?.user.access_token,
+      }),
+      {
+        loading: "Menghapus gambar...",
+        success: () => {
+          setImageUrls((prev) => prev.filter((image) => image.url !== url));
+          return "Gambar berhasil dihapus";
+        },
+        error: (err) => {
+          console.error(err);
+          return "Ups sepertinya ada kesalahan ketika menghapus gambar";
+        },
+      },
+    );
   }
 
   return (
@@ -313,11 +349,11 @@ export default function RosaChatPage() {
           <div className="flex flex-col justify-center gap-6 overflow-y-scroll scrollbar-hide">
             {messages.map((message) => {
               return message.role == "user" ? (
-                <>
-                  <div
-                    className="-mb-4 flex w-auto max-w-max flex-wrap justify-end gap-1 self-end"
-                    key={message.id}
-                  >
+                <div
+                  key={message.chat_id}
+                  className="flex flex-col gap-2 self-end"
+                >
+                  <div className="flex w-auto max-w-max flex-wrap justify-end gap-1 self-end">
                     {message.images?.map((image) => (
                       <Image
                         key={image.image_id}
@@ -332,17 +368,17 @@ export default function RosaChatPage() {
                     ))}
                   </div>
 
-                  <div className="h-max w-auto max-w-[600px] self-end bg-gray/10 p-4 font-medium text-black [border-radius:1.5rem_1.5rem_2px_1.5rem] hover:bg-gray/20">
+                  <div className="h-max w-auto max-w-[600px] bg-gray/10 p-4 font-medium text-black [border-radius:1.5rem_1.5rem_2px_1.5rem] hover:bg-gray/20">
                     {message.content}
                   </div>
-                </>
+                </div>
               ) : (
                 <div
                   className={`mb-4 flex ${message.is_loading ? "items-center" : "items-start"} gap-4`}
-                  key={message.id}
+                  key={message.chat_id}
                 >
                   <Image
-                    src="/img/default-thumbnail.png"
+                    src="/img/ai/APOTEKER-ROSA-1.webp"
                     alt="icon"
                     width={100}
                     height={100}
@@ -350,52 +386,12 @@ export default function RosaChatPage() {
                   />
 
                   <div className="h-max flex-1 font-medium leading-[170%] text-black">
-                    {message.is_loading ? (
-                      <div className="relative max-w-xs rounded-xl bg-gray-100 px-4 py-3 text-gray-700 shadow-sm dark:bg-gray-800 dark:text-gray-200">
-                        <p className="mb-1 text-sm">
-                          {
-                            loadingTexts[
-                              Math.floor(Math.random() * loadingTexts.length)
-                            ]
-                          }
-                        </p>
-                        <div className="flex items-center space-x-1">
-                          <span className="h-2 w-2 animate-pulse rounded-full bg-gray-400 [animation-delay:-0.3s] dark:bg-gray-300"></span>
-                          <span className="h-2 w-2 animate-pulse rounded-full bg-gray-400 [animation-delay:-0.15s] dark:bg-gray-300"></span>
-                          <span className="h-2 w-2 animate-pulse rounded-full bg-gray-400 dark:bg-gray-300"></span>
-                        </div>
+                    {message.is_loading && <ChatLoading />}
 
-                        <div className="absolute -left-2 top-3 h-3 w-3 rotate-45 rounded-sm bg-gray-100 shadow-sm dark:bg-gray-800"></div>
-                      </div>
-                    ) : null}
-
-                    <ReactMarkdown
-                      remarkPlugins={[remarkGfm]}
-                      components={{
-                        ol: ({ children, ...props }) => (
-                          <ol className="list-decimal pl-4" {...props}>
-                            {children}
-                          </ol>
-                        ),
-                        ul: ({ children, ...props }) => (
-                          <ul className="list-disc pl-4" {...props}>
-                            {children}
-                          </ul>
-                        ),
-                        table: ({ children, ...props }) => (
-                          <div className="overflow-x-scroll scrollbar-hide">
-                            <table
-                              className="my-4 table-auto border border-black [&_td]:border [&_td]:p-4 [&_th]:whitespace-nowrap [&_th]:border [&_th]:bg-gray/20 [&_th]:p-4 [&_tr:last-child]:border-b-0 [&_tr]:border-b"
-                              {...props}
-                            >
-                              {children}
-                            </table>
-                          </div>
-                        ),
-                      }}
-                    >
-                      {message.content}
-                    </ReactMarkdown>
+                    <MemoizedMarkdown
+                      id={message.chat_id as string}
+                      content={message.content}
+                    />
                   </div>
                 </div>
               );
@@ -405,18 +401,18 @@ export default function RosaChatPage() {
           </div>
         )}
 
-        <div className="sticky bottom-0 left-0 bg-white pb-4 md:pb-8">
-          <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 bg-gray-100 p-5 shadow-sm">
-            {images.length ? (
+        <div className="sticky bottom-0 left-0 bg-white pb-2 md:pb-4">
+          <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 bg-gray-100 p-4 shadow-sm">
+            {imageUrls.length ? (
               <div className="mb-4 flex flex-wrap gap-2">
-                {images.map((image, index) => (
+                {imageUrls.map((image, index) => (
                   <div
                     key={index}
                     className="relative isolate size-16 overflow-hidden rounded-xl sm:size-20"
                   >
                     <Image
                       priority
-                      src={URL.createObjectURL(image)}
+                      src={image.url}
                       alt="default image"
                       width={100}
                       height={100}
@@ -427,10 +423,8 @@ export default function RosaChatPage() {
                       isIconOnly
                       size="sm"
                       radius="full"
-                      onClick={() =>
-                        setImages((prev) => prev.filter((_, i) => i !== index))
-                      }
                       className="absolute right-1 top-1 z-10"
+                      onClick={() => handleDeleteImage(image.url)}
                     >
                       <X weight="bold" size={18} />
                     </Button>
@@ -462,7 +456,7 @@ export default function RosaChatPage() {
                   ref={fileInputRef}
                   className="hidden"
                   multiple
-                  onChange={handleFileInput}
+                  onChange={handleFileUpload}
                   accept=".jpg, .jpeg, .png"
                 />
 
