28c977f3178f1393ce8b1d5d1825e9e65e2e9f0b
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-07-22 12:18:14 +0700

    feat(components): created card category for videocourse or apotekerclass

---
+++ components/card/CardCategory.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+interface CardCategoryProps {
+  href: string;
+  image: string;
+  name: string;
+  className?: string;
+}
+
+export default function CardCategory({
+  href,
+  image,
+  name,
+  className,
+}: CardCategoryProps) {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        "group relative grid justify-items-center gap-4 overflow-hidden rounded-xl border-2 border-gray/10 p-8 text-sm hover:cursor-pointer hover:bg-purple/10",
+        `${className}`,
+      )}
+    >
+      <Image
+        src={image}
+        alt={name}
+        width={1000}
+        height={1000}
+        className="size-20 object-fill"
+      />
+
+      <h4 className="line-clamp-2 text-center font-extrabold text-black group-hover:line-clamp-none">
+        {name}
+      </h4>
+    </Link>
+  );
+}
