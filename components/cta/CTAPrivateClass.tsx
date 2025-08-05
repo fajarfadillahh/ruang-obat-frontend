317953626fe3cc61d785982796fcd36e88c5bd84
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-05 12:09:04 +0700

    style(component): adjust font size

--- components/cta/CTAPrivateClass.tsx
+++ components/cta/CTAPrivateClass.tsx
@@ -8,7 +8,7 @@ export default function CTAPrivateClass() {
     <section className="[padding:100px_0_156px]">
       <div className="mx-auto flex max-w-[600px] flex-col flex-wrap gap-8 rounded-xl border-2 border-l-[16px] border-black px-6 py-12 sm:px-16 lg:max-w-[700px] lg:flex-row lg:items-center lg:justify-between xl:max-w-[950px]">
         <div className="flex-1 lg:max-w-[500px]">
-          <h2 className="pb-2 text-3xl font-black capitalize -tracking-wide text-black">
+          <h2 className="pb-2 text-2xl font-black capitalize -tracking-wide text-black xs:text-3xl">
             Masih Kesulitan??? Kurang Paham??
           </h2>
 
