d307ab7580a15d31d48115d0ef511ace307210f3
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-11 14:41:50 +0700

    perf(image): load image from storage on quotes for data login & register page

--- data/quotes.ts
+++ data/quotes.ts
@@ -3,45 +3,53 @@ export const quotes = [
     quote:
       "Tujuan pendidikan itu untuk mempertajam kecerdasan, memperkukuh kemauan, serta memperhalus perasaan.",
     figure: "Tan Malaka",
-    image: "/img/pahlawan/tan-malaka.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/tan-malaka.webp",
   },
   {
     quote:
       "Hanya pendidikan yang bisa menyelamatkan masa depan, tanpa pendidikan Indonesia tak mungkin bertahan.",
     figure: "Najwa Shihab",
-    image: "/img/pahlawan/najwa-shihab.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/najwa-shihab.webp",
   },
   {
     quote:
       "Apapun yang dilakukan oleh seseorang itu, hendaknya dapat bermanfaat bagi dirinya sendiri, bermanfaat bagi bangsanya, dan bermanfaat bagi manusia di dunia pada umumnya.",
     figure: "Ki Hadjar Dewantara",
-    image: "/img/pahlawan/ki-hajar-dewantara.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/ki-hajar-dewantara.webp",
   },
   {
     quote:
       "Keberhasilan bukanlah milik orang yang pintar. Keberhasilan adalah kepunyaan mereka yang senantiasa berusaha.",
     figure: "BJ Habibie",
-    image: "/img/pahlawan/bj-habibie.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/bj-habibie.webp",
   },
   {
     quote:
       "Berikan aku 1.000 orang tua, niscaya akan kucabut Semeru dari akarnya. Berikan aku 10 pemuda, niscaya akan kuguncangkan dunia.",
     figure: "Ir. Soekarno",
-    image: "/img/pahlawan/ir-soekarno.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/ir-soekarno.webp",
   },
   {
     quote: "Obat yang paling mujarab adalah ilmu pengetahuan dan kasih sayang.",
     figure: "KH. Agus Salim",
-    image: "/img/pahlawan/kh-agus-salim.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/kh-agus-salim.webp",
   },
   {
     quote: "Hanya dengan pendidikan kita bisa tumbuh menjadi suatu bangsa.",
     figure: "Dewi Sartika",
-    image: "/img/pahlawan/dewi-sartika.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/dewi-sartika.webp",
   },
   {
     quote: "Habis gelap, terbitlah terang.",
     figure: "R.A Kartini",
-    image: "/img/pahlawan/ra-kartini.webp",
+    image:
+      "https://ruangobat.is3.cloudhost.id/statics/images/pahlawan-img/ra-kartini.webp",
   },
 ];
